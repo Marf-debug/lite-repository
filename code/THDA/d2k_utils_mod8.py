f975dab715355714b771e02500f7b288474dd994
Marf-debug <70505698+Marf-debug@users.noreply.github.com>
2026-04-23 13:38:19 +1000

    Update d2k_utils_mod8.py

--- code/THDA/d2k_utils_mod8.py
+++ code/THDA/d2k_utils_mod8.py
@@ -247,8 +247,8 @@ def create_crosstab(var, target='fail_ext_status_bin'):
         ]},
         # Highlight the totals row with a light gray background
         {'selector': 'tr:last-child', 'props': [
-            ('background-color', 'Canvas'),
-            ('color', 'CanvasText')
+            ('background-color', '#555'),
+            ('color', 'white')
         ]}
     ])
     
